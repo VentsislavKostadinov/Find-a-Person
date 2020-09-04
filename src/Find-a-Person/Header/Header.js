2a3b092451130eaca91b252b30c71266ed63da75
VentsislavKostadinov <ventsislav_kostadinov@abv.bg>
2020-09-04 18:08:36 +0100

    first-steps

---
+++ src/Find-a-Person/Header/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './style/style.css';
+const Header = () => {
+    return (
+        <header>
+            {/* <img className= 'img-fluid' src={headerImg2} alt= 'img' /> */}
+            {/*   <Image src= {headerImg2} fluid /> */}
+            <h2 className= 'h2'>Find a Person</h2>
+        </header>
+    )
+}
+
+export default Header
